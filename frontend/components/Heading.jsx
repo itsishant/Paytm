79bcd2aec1193e032f30cb2552c357f9a37d843e
itsishant <modakmania@gmail.com>
2025-05-07 22:13:30 +0530

    added some frontend

---
+++ frontend/components/Heading.jsx
@@ -0,0 +1,7 @@
+export const Heading = ({label}) => {
+    return(
+        <div className="font-bold text-black text-5xl pt-6">
+            {label}
+        </div>
+    )
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "../pages/Signup";
+import { Signin } from "../pages/Signin";
 
-function App() {
 
-  return (
+const App = () => {
+
+  return(
     <div>
-        Hello world
+      <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        {/* <Route path="/signin" element={} />
+        <Route path="/dashboard" element={} />
+        <Route path="/send" element={} /> */}
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
-export default App
+export default App;
